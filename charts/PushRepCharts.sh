acef251267d5a55501ad1f0eeb387a242010d9c2
ffougere <fabrice.fougere@gmail.com>
2023-07-01 08:25:02 -0400

    Test

--- charts/PushRepCharts.sh
+++ charts/PushRepCharts.sh
@@ -4,8 +4,8 @@ echo "Input1: $1"
 echo "CommitVar: $1"
 
 cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/charts
-helm package charts/selenium
-helm repo index --url https://github.com/ffougere/opcito --merge index.yaml .
+helm package charts/vault
+helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
 git add .
 git commit -m "$CommitVar"
 git push
