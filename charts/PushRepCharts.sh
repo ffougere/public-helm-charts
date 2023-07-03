e7dee69b985f2ba971e4a3060d70a4fe55d0002e
ffougere <fabrice.fougere@gmail.com>
2023-07-03 10:24:21 -0400

    Add

--- charts/PushRepCharts.sh
+++ charts/PushRepCharts.sh
@@ -4,7 +4,7 @@ echo "Input1: $1"
 echo "CommitVar: $1"
 
 cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/charts
-helm package charts/vault
+helm package charts/prometheus
 helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
 git add .
 git commit -m "$CommitVar"
