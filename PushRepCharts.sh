a947e50441bf129904e3b3574c14f5f42b4bba10
ffougere <fabrice.fougere@gmail.com>
2023-08-24 12:27:05 -0400

    selenium 11

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -12,3 +12,11 @@ git add .
 git commit -m "$CommitVar"
 git push
 git status
+
+cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/charts
+helm package selenium2
+helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
+git add .
+git commit -m "selenium 10"
+git push
+git status
