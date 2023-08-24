c2fa559aff1b513087d068a9cb25178d31a06137
ffougere <fabrice.fougere@gmail.com>
2023-08-24 13:42:33 -0400

    selenium 11

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -14,7 +14,7 @@ git push
 git status
 
 cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/
-helm package charts/selenium2
+helm package charts/selenium3
 helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
 git add .
 git commit -m "selenium 11"
