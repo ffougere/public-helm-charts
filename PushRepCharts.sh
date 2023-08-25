b2e049a683f3b323426f7e375b521c3da7b4ffdb
ffougere <fabrice.fougere@gmail.com>
2023-08-25 16:38:10 -0400

    kyverno 1

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -20,3 +20,11 @@ git add .
 git commit -m "selenium new 4"
 git push
 git status
+
+cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/
+helm package charts/kyverno
+helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
+git add .
+git commit -m "kyverno 1"
+git push
+git status
