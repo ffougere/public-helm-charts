d6e306a4353c65a2972dd29fd69ad74940e2b57e
ffougere <fabrice.fougere@gmail.com>
2023-08-28 16:55:41 -0400

    prometheus 1

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -28,3 +28,11 @@ git add .
 git commit -m "kyverno 1"
 git push
 git status
+
+cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/
+helm package charts/prometheus
+helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
+git add .
+git commit -m "prometheus 1"
+git push
+git status
