5881b936822d832492b15e961ca6ef0f17e6fa6f
ffougere <fabrice.fougere@gmail.com>
2023-08-28 18:45:59 -0400

    prometheus 3

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -33,6 +33,6 @@ cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/
 helm package charts/prometheus
 helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
 git add .
-git commit -m "prometheus 2"
+git commit -m "prometheus 3"
 git push
 git status
