0296b0c6dca06bcee586fdb2e474f328d78c1479
ffougere <fabrice.fougere@gmail.com>
2023-08-28 22:00:25 -0400

    grafana 1

--- PushRepCharts.sh
+++ PushRepCharts.sh
@@ -36,3 +36,11 @@ git add .
 git commit -m "prometheus 3"
 git push
 git status
+
+cd /appli/ffougere-repo/public-helm-charts/public-helm-charts/
+helm package charts/grafana
+helm repo index --url https://github.com/ffougere/public-helm-charts/charts --merge index.yaml .
+git add .
+git commit -m "grafana 1"
+git push
+git status
